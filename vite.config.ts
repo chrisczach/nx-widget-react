cfcce3260b8859b841e2d62ff63a9ea27144c55c
Chris Czach <czach@networkoptix.com>
2022-02-14 13:33:45 -0800

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { generateConfig } from 'widget-dashboard-tools'
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(generateConfig({
+  devServer: 'http://127.0.0.1:3000',
+  cloudInstance: 'https://dev3.cloud.hdw.mx',
   plugins: [react()]
-})
+}))
